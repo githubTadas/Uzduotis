5e8959eaf985c4ad1361ac4eb3653d00dbedcbfd
githubTadas <taduii@gmail.com>
2018-07-31 22:20:32 +0300

    first commit

---
+++ src/OOP/controller/TAnimals.java
@@ -0,0 +1,4 @@
+package OOP.controller;
+
+public interface TAnimals {
+}
---
+++ src/OOP/view/AnimalView.java
@@ -0,0 +1,11 @@
+package OOP.view;
+
+import OOP.model.Animal;
+import OOP.model.Animals;
+
+public class GyvunasView implements Animals {
+
+
+
+}
+
